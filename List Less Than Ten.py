22e96208e7483a4622da10d9545bebf4db0e66bb
omar7164 <36710003+omar7164@users.noreply.github.com>
2018-03-08 13:20:58 -0800

    Delete List Less Than Ten.py

--- List Less Than Ten.py
+++
@@ -1,8 +0,0 @@
-# Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-num = int(input("Choose a number: "))
-new_list = []
-for i in a:
-	if i < num:
-		new_list.append(i)
-print (new_list)
