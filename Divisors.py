5030ee673902fed6b4eadcd998fbfee2199d9a5b
omar al-zahawi <omar7164@gmail.com>
2018-03-08 22:49:03 -0800

    this is phase 1

---
+++ Divisors.py
@@ -0,0 +1,14 @@
+#asks the user for a number and then prints out a list of all the divisors of that number.
+__author__ = 'jeffreyhunt'
+
+num = int(input("Please choose a number to divide: "))
+
+listRange = list(range(1,num+1))
+
+divisorList = []
+
+for number in listRange:
+    if num % number == 0:
+        divisorList.append(number)
+
+print(divisorList)
